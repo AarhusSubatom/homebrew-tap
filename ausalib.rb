526493243804ac0d2ab26070c4bca84476acf75d
GitLab Runner <gitlab_ci_multi_runner@kern.phys.au.dk>
2016-03-19 18:34:23 +0100

    Updated to ausalib.rb to version 1

--- ausalib.rb
+++ ausalib.rb
@@ -1,11 +1,11 @@
 class Ausalib < Formula
 
-  version_number = "16.3"
+  version_number = "1"
   desc "AarhusSubatom analysis library"
   homepage "https://git.kern.phys.au.dk/ausa/ausalib"
   url "https://git.kern.phys.au.dk/ausa/ausalib/repository/archive.tar.gz?ref=#{version_number}"
   version version_number
-  sha256 "eb0238d1c4ce3e76a3cd032d80520a08b33af85eb50c5cd25d3679a886167325"
+  sha256 "cdc867160b4703f5c4f1411dee07e9c87e8fa18c53dc83a94fbb32b73f73a72e"
   head "https://git.kern.phys.au.dk/ausa/ausalib.git"
 
   option 'with-root-6', 'Install with ROOT 6'
