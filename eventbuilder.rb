010d452055c18e2a58acc95d455d78e2449f9378
Munken <mm.munk@gmail.com>
2017-01-15 18:18:57 +0100

    Forgot eventbuilder recipe

---
+++ eventbuilder.rb
@@ -0,0 +1,27 @@
+class Eventbuilder < Formula
+
+  version_number = "16.3"
+  desc "AUSAlib event builder"
+  homepage "https://git.kern.phys.au.dk/ausa/EventBuilder"
+  url "https://git.kern.phys.au.dk/ausa/EventBuilder/repository/archive.tar.gz?ref=#{version_number}"
+  version version_number
+  sha256 "942c5b67409fc650dc099dae41c76633f3a5d11f6086b50aac486a3dba484309"
+  head "https://git.kern.phys.au.dk/ausa/EventBuilder.git"
+
+  option 'with-root-6', 'Install with ROOT 6'
+
+  depends_on 'root'  if !build.with?('root-6')
+  depends_on 'root6' if  build.with?('root-6')
+  depends_on "cmake" => :build
+  depends_on "ausalib" => :build
+  needs :cxx11 
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
